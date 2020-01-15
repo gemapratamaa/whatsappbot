3acaf701390589d9d89ab4b5e6c5ab72a3b70e20
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-01-16 06:30:55 +0700

    test index page

--- bot.py
+++ bot.py
@@ -150,6 +150,11 @@ def randomQuote():
     randomQuote = random.choice(result)['text']
     msg.body(randomQuote)
 
+@app.route('/')
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+    
 if __name__ == "__main__":
     #port = int(os.environ.get('PORT', 5000))
     #app.run(host='127.0.0.1', port=port)
