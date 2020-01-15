ee43c143f1575f99593737eaa56405117f40b119
Gema Pratama Aditya <gemapratamaaditya@yahoo.com>
2020-01-16 06:20:19 +0700

    change to deployment

--- bot.py
+++ bot.py
@@ -151,8 +151,9 @@ def randomQuote():
     msg.body(randomQuote)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='127.0.0.1', port=port)
+    #port = int(os.environ.get('PORT', 5000))
+    #app.run(host='127.0.0.1', port=port)
+    app.run(threaded=True, port=5000)
 
 
     """
